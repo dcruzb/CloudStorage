1bd99b71883dee1728ff57aec2dfb142c87a692e
Daniel Brandão <dcb@cin.ufpe.br>
2019-06-01 20:17:04 -0300

    Criada interface de storage com funções para implementação nos serviços especificos de nuvem.

---
+++ server/lib/StorageFunctions.go
@@ -0,0 +1,21 @@
+package lib
+
+import (
+	"os"
+	"time"
+)
+
+type StorageFunctions interface {
+	SendFile(file *os.File) (createdFile CloudFile, err error)
+	GetFile() (file *os.File, err error)
+	List(path string) (files []CloudFile, err error)
+}
+
+type CloudFile struct {
+	Id          string
+	Path        string
+	Cloud       string
+	Size        string
+	Created     time.Time
+	LastChecked time.Time
+}
