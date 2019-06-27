c65332bc0c88d53670d3e2077ba1b8a9debafd7d
Daniel Brandão <dcb@cin.ufpe.br>
2019-06-27 02:06:20 -0300

    Ajuste da assinatura para bater com alteração da interface

--- cloudLib/aws/awsFunctions.go
+++ cloudLib/aws/awsFunctions.go
@@ -21,16 +21,16 @@ func (AwsFunctions) List(path string) (files []cloudLib.CloudFile, err error) {
 	panic("implement me")
 }
 
-func (AwsFunctions) Price(size float64) float64 {
+func (AwsFunctions) Price(size float64) (price float64, err error) {
 	// Todo usar cache dos dados
 	// Todo criar função para preencher o cache
 	// Todo obter arquivo data.json diretamente da AWS
 
 	aws := awsLib.Aws{}
 
-	return aws.Price(size)
+	return aws.Price(size), nil
 }
 
-func (AwsFunctions) Availability() bool {
-	return false
+func (AwsFunctions) Availability() (available bool, err error) {
+	return true, nil
 }
