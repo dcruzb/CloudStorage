9b8fea90f390ff52d9c3865d9869993d328c4442
Daniel Brandão <dcb@cin.ufpe.br>
2019-06-29 18:08:26 -0300

    Implementada estrutura básica, sem funcionalidade, somente um mock para simular google como sendo mais barata que a aws

--- cloudLib/google/googleAPI/googleAPI.go
+++ cloudLib/google/googleAPI/googleAPI.go
@@ -1,1 +1,28 @@
 package googleAPI
+
+import (
+	"CloudStorage/cloudLib"
+	"os"
+)
+
+type Google struct {
+}
+
+func (Google) Price(size float64) float64 {
+	return 3.1234
+}
+
+func (Google) SendFile(file *os.File, path string) (createdFile cloudLib.CloudFile, err error) {
+
+	return createdFile, nil
+}
+
+func (Google) GetFile(fileName string, path string) (file *os.File, err error) {
+
+	return file, nil
+}
+
+func (Google) List(path string) (files []cloudLib.CloudFile, err error) {
+
+	return files, nil
+}
