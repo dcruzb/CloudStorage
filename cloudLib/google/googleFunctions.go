2d6083a25e93c3b055270a30a096948b590b100d
Daniel Brandão <dcb@cin.ufpe.br>
2019-06-29 18:09:19 -0300

    Finalizada implementação do objeto remoto para Google

--- cloudLib/google/googleFunctions.go
+++ cloudLib/google/googleFunctions.go
@@ -1,12 +1,37 @@
 package google
 
+import (
+	"CloudStorage/cloudLib"
+	"CloudStorage/cloudLib/google/googleAPI"
+	"os"
+)
+
 type GoogleFunctions struct {
 }
 
-func (gf GoogleFunctions) Price(size float64) float64 {
-	return size + 10
+func (gf GoogleFunctions) SendFile(file *os.File, path string) (createdFile cloudLib.CloudFile, err error) {
+	google := googleAPI.Google{}
+	return google.SendFile(file, path)
 }
 
-func (gf GoogleFunctions) Availability() bool {
-	panic("implement me")
+func (gf GoogleFunctions) GetFile(fileName string, path string) (file *os.File, err error) {
+	google := googleAPI.Google{}
+	return google.GetFile(fileName, path)
+}
+
+func (gf GoogleFunctions) List(path string) (files []cloudLib.CloudFile, err error) {
+	google := googleAPI.Google{}
+	return google.List(path)
+}
+
+func (gf GoogleFunctions) Price(size float64) (price float64, err error) {
+	// Todo usar cache dos dados
+	// Todo criar função para preencher o cache
+	google := googleAPI.Google{}
+
+	return google.Price(size), nil
+}
+
+func (gf GoogleFunctions) Availability() (available bool, err error) {
+	return true, nil
 }
